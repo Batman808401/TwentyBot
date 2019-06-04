fb6e54e6ae38bd24a4d6b7fdc8880d22d772bdbc
Edgardo Velazquez <edv9298@hotmail.com>
2019-06-03 20:24:03 -0400

    Update app.js
    Deleted old comments

--- app.js
+++ app.js
@@ -8,26 +8,12 @@ bot.on('ready', () => {
 })
 
 bot.on('message', msg => {
-	//if (msg.author.bot || !msg.content.startsWith(cfg.prefix)) return;
-	/*const args = msg.content.slice(cfg.prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
-	console.log(`Args: ${args}\nCommand: ${command}`);
-
-	if (command === 'boop') {
-		msg.channel.send('*boop*')
-	}
-
-	if (msg.content === 'penis') {
-		msg.reply('u sus boi')
-	}*/
 	if (msg.content === 'penis' || msg.content === 'dick') {
 		msg.reply('u sus boi')
 	}
 
 	console.log(msg.author.username + ': ' + msg.content);
-	//console.log(user('<@184326588655992832>').presence.status)
 
 })
 
 bot.login(cfg.token)
-//console.log(cfg);

2f5fb757c0250c10701b0fd8fbce9ea50ca3f7e2
Edgardo Velazquez <edv9298@hotmail.com>
2019-06-04 22:27:44 -0400

    Fixed crashes
    - Fixed crashing when a server member goes offline
    - Also, added comments
    
    Need to fix repeated status reports

--- app.js
+++ app.js
@@ -16,12 +16,26 @@ bot.on('message', msg => {
 	console.log(msg.author.username + ': ' + msg.content);
 })
 
+//event when any user changes their presence
 bot.on("presenceUpdate", (oldMember, newMember) => {
-	if (newMember.presence.game.streaming == true) {
-		bot.channels.get("585613596134735874").send(newMember.displayName + " is now live!")
+
+	//A string that gives the User and their discord status
+	const generalStatus = newMember.displayName + " is now " + newMember.presence.status;
+
+	//check if User went offline
+	if (newMember.presence.status === "offline") {
+		console.log(generalStatus)
 	} else {
-		console.log(newMember.displayName + " is not streaming");
+		//Check if the user is streaming
+		if (newMember.presence.game.streaming == true) {
+			//if the user is streaming
+			bot.channels.get("585613596134735874").send(newMember.displayName + " is now live!")
+		} else {
+			//state the status of the user
+			console.log(generalStatus);
+		}
 	}
+
 })
 
 bot.login(cfg.token)
